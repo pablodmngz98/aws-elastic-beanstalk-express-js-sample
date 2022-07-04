0664d45fcbd09b8fe11defd089d95dcff144cdca
Pablo <pablo.dominguez@galgus.net>
2022-07-04 15:00:03 +0200

    change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('¡Tres tristes tigres comen trigo en un trigal!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
