afb31e120709336b4e4b429171b9ee2b53fe04b5
Pablo <pablo.dominguez@galgus.net>
2022-07-05 11:23:32 +0200

    Full pipeline test

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => res.send('¡Tres tristes tigres comen trigo en un trigal!'));
+app.get('/', (req, res) => res.send('Buen trabalenguas'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
